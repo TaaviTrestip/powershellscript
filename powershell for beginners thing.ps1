580965aa4daa76dab4e7665635134b0d0ce225d8
Taavi Trestip <taavi.trestip@voco.ee>
2026-02-03 09:24:24 +0200

    variable and data types

---
+++ powershell for beginners thing.ps1
@@ -0,0 +1,30 @@
+﻿Write-Host "I want to learn Powershell" -ForegroundColor Green
+
+Write-Host "Variable" -ForegroundColor Cyan
+# Variable
+10+4
+10-4
+10*4
+
+$a = 10
+$b = 4
+$a+$b
+$a-$b
+$a*$b
+
+Write-Host "Data Types" -ForegroundColor Cyan
+# Data Types
+$v = 1 + 1
+$v
+$v.GetType()
+
+$v = '1' + '1'
+$v
+$v.GetType()
+
+$v = 1.9
+[int32]$v
+[float]$v
+[string]$v
+[boolean]$v
+[datetime]$v
