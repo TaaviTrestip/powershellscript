f5adf90d5e358a0f4ddb43f61c22568f87c08ec6
Taavi Trestip <taavi.trestip@voco.ee>
2026-02-07 16:04:29 +0200

    read and write host

---
+++ powershell for beginners/read and write host.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host "Read-Host and Write-Host" -ForegroundColor Cyan
+$t = Read-Host "What is the time"
+$t
+
+$t = Read-Host "What is the time"
+$t
+
+Write-Host "It is a good day" -ForegroundColor Green
+Write-Host "It is a good day" -ForegroundColor Yellow
+Write-Host "It is a good day"
+Write-Host "It is a good day" -ForegroundColor Cyan
+
+Write-Host "Practice 3" -ForegroundColor Red
+
+$name = Read-Host "What is your name?"
+Write-Host "Your name is: $name" -ForegroundColor Green
