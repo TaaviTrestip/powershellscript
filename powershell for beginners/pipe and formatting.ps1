0e47d51b97da82144ba4615ee16dac37b96960ad
Taavi Trestip <taavi.trestip@voco.ee>
2026-02-07 15:58:55 +0200

    pipeline and formatting

---
+++ powershell for beginners/pipe and formatting.ps1
@@ -0,0 +1,33 @@
+﻿Write-Host "Pipeline or pipe and formatting" -ForegroundColor Cyan
+Get-Date
+
+Get-Date | Fl
+
+Get-Date | Format-List
+
+Get-Service BITS | Format-List
+
+Get-Service BITS | Format-List *
+
+Get-Service BITS | Format-List *name*
+
+Get-Date | Format-Table
+
+Get-Date | FT
+
+Get-Service BITS | Format-Table
+
+Get-Service BITS | Format-Table -AutoSize
+
+Get-Service BITS | Format-Table -Auto
+
+Get-Service a*
+
+Get-Service a* | Sort-Object Status
+
+Get-Service EventLog
+
+Get-Service EventLog | fl
+
+$service = Get-Service EventLog | Select Name,Status
+$service
