1e69014d5266c3f5a1634c169941c4b107cd9dd6
Taavi Trestip <taavi.trestip@voco.ee>
2026-02-07 15:41:32 +0200

    practice 2

--- powershell for beginners thing.ps1
+++ powershell for beginners thing.ps1
@@ -35,6 +35,7 @@ $nameArray = @("John Pork","Joe Mama","crocodilobombardino")
 $nameArray[0]
 $nameArray[1]
 $nameArray[2]
+Write-Host "Practice 1" -ForegroundColor Red
 # Practice 1
 $array1 = @(1, 2, 3)
 $array2 = @(4, 5, 6)
@@ -46,3 +47,28 @@ $array3 += $array1[1] + $array2[1]
 $array3 += $array1[2] + $array2[2]
 
 $array3
+
+# Hashtable
+Write-Host "Hashtables" -ForegroundColor Cyan
+Write-Host "First Hashtable" -ForegroundColor Cyan
+$user=@{FirstName="John"; LastName="Smith"; MiddleInitial="J"; Age=40}
+$user.FirstName
+$user.LastName
+$user.MiddleInitial
+$user.Age
+
+Write-Host "Second Hashtable" -ForegroundColor Cyan
+$user=@{FirstName="John","Joe","Garry"; LastName="Smith","Parker","Smith"; MiddleInitial="D","L","N"; Age=40,32,25}
+$user
+$user.Age
+$user.FirstName
+$user.Lastname
+
+Write-Host "Practice 2" -ForegroundColor Red
+$daysworked = @{Name="John","Joe","Mary";DaysWorked=12,20,18}
+$salary = @{Name="John","Joe","Mary";SalaryPerDay=100,120,150}
+$johnsalary = $daysworked.DaysWorked[0] * $salary.SalaryPerDay[0]
+$joesalary = $daysworked.DaysWorked[1] * $salary.SalaryPerDay[1]
+$marysalary = $daysworked.DaysWorked[2] * $salary.SalaryPerDay[2]
+$finalsalary = @{Name=$daysworked.Name[0],$daysworked.Name[1],$daysworked.Name[2];Salary=$johnsalary,$joesalary,$marysalary}
+$finalsalary
