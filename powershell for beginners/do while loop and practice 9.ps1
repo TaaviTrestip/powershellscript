e2c27c5f07266d047be4872ac6f2f3a1ba199a28
Taavi Trestip <taavi.trestip@voco.ee>
2026-02-18 13:25:19 +0200

    do while loop and practice 9

---
+++ powershell for beginners/do while loop and practice 9.ps1
@@ -0,0 +1,26 @@
+﻿# Do While Loop
+Write-Host "Do While Loop" -ForegroundColor Cyan
+
+$i = 0
+do
+{
+    Write-Host "Current Value is: " $i -ForegroundColor Cyan
+    $i++
+}while($i -lt 3)
+
+# Practice 9
+Write-Host "Practice 9" -ForegroundColor Red
+
+$count = 0
+
+do {
+    if (Get-Process -Name notepad -ErrorAction SilentlyContinue) {
+        Write-Host "Notepad is running"
+        $count++
+        Start-Sleep -Seconds 1
+    }
+}
+while (Get-Process -Name notepad -ErrorAction SilentlyContinue)
+
+Write-Host "Notepad is closed."
+Write-Host "The statement was displayed $count times."
