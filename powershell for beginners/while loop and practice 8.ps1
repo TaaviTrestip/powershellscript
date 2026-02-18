0163cfbd7412e499b35cda15fc978c0d8214ca9f
Taavi Trestip <taavi.trestip@voco.ee>
2026-02-18 13:20:28 +0200

    while loop and practice 8

---
+++ powershell for beginners/while loop and practice 8.ps1
@@ -0,0 +1,19 @@
+﻿# While loop
+Write-Host "While Loop" -ForegroundColor Cyan
+
+$i = 0
+while($i -lt 3)
+{
+    Write-Host "Current Value is: " $i -ForegroundColor Yellow
+    $i++
+}
+
+# Practice 8
+Write-Host "Practice 8" -ForegroundColor Red
+
+while (Get-Process -Name notepad -ErrorAction SilentlyContinue) {
+    Write-Host "Notepad is running"
+    Start-Sleep -Seconds 2
+}
+
+Write-Host "Notepad is closed."
