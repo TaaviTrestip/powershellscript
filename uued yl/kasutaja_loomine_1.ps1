947cbbc85cf405d81924d0a08ae55ad947468b19
Taavi Trestip <taavi.trestip@voco.ee>
2026-03-02 15:20:39 +0200

    lokaalsed kasutajad

---
+++ uued yl/kasutaja_loomine_1.ps1
@@ -0,0 +1,27 @@
+﻿# Defineerime parameetrid
+#param(
+#$kasutajanimi,
+#$taisnimi,
+#$kontokirjeldus
+#)
+#$kasutajanimi=$args[0]
+#$taisnimi=$args[1]
+#$kontokirjeldus=$args[2]
+
+# Kontrollime parameetreid
+if($args.Count -ne 3){
+    echo '.\SkriptiNimi kasutajanimi "Ees Perenimi" "Konto kirjeldus"'
+} else {
+    $kasutajanimi=$args[0]
+    $taisnimi=$args[1]
+    $kontokirjeldus=$args[2]
+    # Parool
+    $KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+    # Lisame kasutaja
+    New-LocalUser "$kasutajanimi" -Password $KasutajaParool -FullName "$taisnimi" -Description "$kontokirjeldus"
+}
+
+# Parool
+#$KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+# Lisame kasutaja
+#New-LocalUser "$kasutajanimi" -Password $KasutajaParool -FullName "$taisnimi" -Description "$kontokirjeldus"
