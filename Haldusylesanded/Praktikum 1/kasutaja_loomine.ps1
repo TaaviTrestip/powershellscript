a7a3ea41c5c511e26c9dcd3f12f6941ddb557eb7
Taavi Trestip <taavi.trestip@voco.ee>
2026-03-23 19:15:29 +0200

    restructure for files

---
+++ Haldusylesanded/Praktikum 1/kasutaja_loomine.ps1
@@ -0,0 +1,14 @@
+﻿Get-Command -Module Microsoft.PowerShell.LocalAccounts
+
+Get-LocalUser
+
+Get-LocalUser -Name "Taavi" | Select-Object *
+Get-LocalUser -Name "Taavi" | Select-Object PasswordLastSet
+
+# New-LocalUser "kasutaja1"
+
+Remove-LocalUser -Name kasutaja1 -Verbose
+
+$KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+
+New-LocalUser "kasutaja1" -Password $KasutajaParool -FullName "Esimene Kasutaja" -Description "Local Account - kasutaja1"
---
+++ Haldusylesanded/Praktikum 1/kasutaja_loomine_2.ps1
@@ -0,0 +1,14 @@
+﻿# Fail, kus sees kasutajate andmed
+$file="C:\Users\Taavi\powershellscript\uued yl\kasutajad.csv"
+# Importime .csv faili korrektselt
+$Kasutajad=Import-Csv $file -Encoding Default -Delimiter ";"
+# Lisame iga kasutaja kasutades ForEach loopi
+foreach ($kasutaja in $Kasutajad)
+{
+    $kasutajanimi = $kasutaja.Kasutajanimi
+    $taisnimi = $kasutaja.Taisnimi
+    $kontokirjeldus = $kasutaja.KontoKirjeldus
+    $parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+    # Kasutades siis saadud infot lisame kasutaja
+    New-LocalUser "$kasutajanimi" -Password $parool -FullName "$taisnimi" -Description "$kontokirjeldus"
+}
